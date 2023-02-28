bd1e2f7288604acd7296059f1a52d0a09d62ec4f
Vishal Mangela <vishalmangela@cloudxsupport.com>
2023-02-28 12:49:04 +0530

    version4

--- test_vm.tf
+++ test_vm.tf
@@ -1,3 +1,16 @@
+terraform {
+required_providers {
+azurerm = {
+source = "hashicorp/azurerm"
+version = "3.45.0"
+}
+}
+}
+provider "azurerm" {
+#
+features {}
+
+}
 resource "azurerm_resource_group" "example" {
   name     = "TF-DEMOVM"
   location = "central India"
