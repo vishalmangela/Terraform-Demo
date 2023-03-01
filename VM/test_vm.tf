5abe73778658cbaf080c2001ac59b2fb12902c80
Vishal Mangela <vishalmangela@cloudxsupport.com>
2023-03-01 11:50:26 +0530

    v2

--- VM/test_vm.tf
+++ VM/test_vm.tf
@@ -31,7 +31,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_network_interface" "example" {
-  name                = "TFDEMO-nics"
+  name                = "TFDEMO-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
